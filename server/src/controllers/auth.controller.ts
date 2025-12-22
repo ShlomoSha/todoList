4f0ca0fef1aedd5f2c0c4a0e23fc8dd566ad6a1f
shlomoShalem <s0552288304@gmail.com>
2025-12-22 23:40:34 +0200

    initial auth.controllers and done auth.router

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,23 @@
+export const register = async () => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+export const login = async () => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+export const validateToken = async () => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
---
+++ server/src/routes/auth.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { login, register, validateToken } from "../controllers/auth.controller";
+
+const authRouter = Router()
+
+authRouter.post("register", register)
+authRouter.post("login", login)
+authRouter.post("validate-token", validateToken)
+
+export default authRouter
