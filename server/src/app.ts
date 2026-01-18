61f3e1a6b44bd67109c1ea7f8cb54ff45623a651
ShlomoSha <shlomoworking1@gmail.com>
2026-01-18 13:59:07 +0200

    added app.use for two middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import authRouter from './routes/auth.router'
+import { errorHandler, notFoundHandler } from './middleware/errorHandler'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/auth', authRouter)
+
+app.use(notFoundHandler)
+app.use(errorHandler)
