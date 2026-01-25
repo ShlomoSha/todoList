03b7e9b154c73e9a59628da616382bc043be7c68
ShlomoSha <shlomoworking1@gmail.com>
2026-01-25 12:09:24 +0200

    added some app.use

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,24 @@
 import express from 'express'
+import helmet from 'helmet'
 import cors from 'cors'
 import authRouter from './routes/auth.router'
 import { errorHandler, notFoundHandler } from './middleware/errorHandler'
 
-export const app = express()
+const app = express()
 
-app.use(express.json())
+app.use(helmet())
 app.use(cors())
+app.use(express.json())
 
+app.get('/ping', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'server is running'
+    })
+})
 app.use('/auth', authRouter)
 
 app.use(notFoundHandler)
 app.use(errorHandler)
+
+export default app
