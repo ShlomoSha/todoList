0abf879f8fbb23fea02fbea84e302967c86c919f
ShlomoSha <shlomoworking1@gmail.com>
2026-01-28 12:49:58 +0200

    fix the message

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -6,7 +6,7 @@ export const register = asyncHandler(async (req: Request, res: Response) => {
     const newUser = await createNewUser(req.body)
     res.status(201).json({
         success: true,
-        message: 'User register successfuly',
+        message: 'User register successfully',
         data: { user: newUser }
     })
 })
@@ -15,7 +15,7 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
     const loginData = await userLogin(req.body)
     res.status(201).json({
         success: true,
-        message: 'User logged in  successfuly',
+        message: 'User logged in  successfully',
         data: { loginData }
     })
 })
