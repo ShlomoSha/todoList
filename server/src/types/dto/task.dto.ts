c3225f86005a7eedfdd3cf647b4e40dd906c0b69
ShlomoSha <shlomoworking1@gmail.com>
2026-01-28 12:50:47 +0200

    remove the assingedTo from dto

--- server/src/types/dto/task.dto.ts
+++ server/src/types/dto/task.dto.ts
@@ -1,16 +1,14 @@
-import ITask from "../../models/tasks/tasks.interface";
+import ITask from "../../models/tasks/task.interface";
 
 export interface CreateTaskDTO {
     title: string;
     description?: string;
-    assignedTo: string;
 }
 
 export interface UpdateTaskDTO {
     title?: string;
     description?: string;
     isCompleted?: boolean;
-    assignedTo?: string;
 }
 
 export interface TaskResponseDTO extends ITask {
