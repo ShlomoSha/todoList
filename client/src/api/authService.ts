7ab287fbe0d9d45a5ba11dd81d281fa1c08133b0
ShlomoSha <shlomoworking1@gmail.com>
2026-02-09 11:47:27 +0200

    use api helper in auth serivce

--- client/src/api/authService.ts
+++ client/src/api/authService.ts
@@ -1,13 +1,16 @@
 import { ROUTES, TOKEN } from "../constants/constants";
-import type { AuthDto } from "../types/auth.interface";
+import type { AuthDto } from "../types/auth.dto";
+import createEndpoint from "./apiHelper.utlis";
 import axiosInstance from "./axiosConfig";
 
+const authEndpoint = createEndpoint(ROUTES.AUTH)
+
 const authService = {
-    login: (credentials: AuthDto) => axiosInstance.post(`/${ROUTES.AUTH}/${ROUTES.LOGIN}`, credentials),
-    register: (userData: AuthDto) => axiosInstance.post(`/${ROUTES.AUTH}/${ROUTES.REGISTER}`, userData),
+    login: (credentials: AuthDto) => axiosInstance.post(authEndpoint(ROUTES.LOGIN), credentials),
+    register: (userData: AuthDto) => axiosInstance.post(authEndpoint(ROUTES.REGISTER), userData),
     logout: async () => {
         localStorage.removeItem(TOKEN)
-    }
+    },
 }
 
 export default authService
