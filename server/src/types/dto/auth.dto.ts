d7f7bdb22a74e3e816b7e3508f6b844c7cb65473
ShlomoSha <shlomoworking1@gmail.com>
2026-02-12 15:56:47 +0200

    added field for reset password

--- server/src/types/dto/auth.dto.ts
+++ server/src/types/dto/auth.dto.ts
@@ -6,3 +6,8 @@ export interface LoginDTO {
 export interface RegisterDTO extends LoginDTO {
     email: string
 }
+
+export interface ResetPasswordDTO {
+    rawToken: string;
+    newPassword: string;
+}
