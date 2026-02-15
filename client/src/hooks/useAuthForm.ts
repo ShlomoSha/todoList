7313131071b0cfbc8c714c6902826a4d411237f9
ShlomoSha <shlomoworking1@gmail.com>
2026-02-15 14:55:04 +0200

    created hook that contain the field and function for all auth form

---
+++ client/src/hooks/useAuthForm.ts
@@ -0,0 +1,79 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import authService from "../api/authService"
+import { ROUTES, TOKEN } from "../constants/constants"
+
+interface UseAuthFormProps {
+    isLogin: boolean
+}
+
+export function useAuthForm({ isLogin }: UseAuthFormProps) {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
+
+    const navigateTo = useNavigate()
+
+    const validateForm = () => {
+        if (username === '' || password === '') return false
+        if (!isLogin && email === '') return false
+        return true
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setLoading(true)
+        setSubmitted(true)
+        setError('')
+
+        if (!validateForm()) {
+            setError('Something is missing')
+            setLoading(false)
+            return
+        }
+
+        try {
+            if (isLogin) {
+                const response = await authService.login({ username, password })
+                localStorage.setItem(TOKEN, response.data.token)
+                navigateTo(`/${ROUTES.TASKS}`)
+            } else {
+                await authService.register({ username, email, password })
+                navigateTo(`/${ROUTES.LOGIN}`)
+            }
+
+        } catch (err: any) {
+            setError(err.response?.data.message || 'Server Error. Please try again later.')
+            console.error(err.response?.data.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const clearError = () => setError('')
+
+    return {
+        // Form state
+        username,
+        email,
+        password,
+        error,
+        loading,
+        submitted,
+        showPassword,
+        
+        // Setters
+        setUsername,
+        setEmail,
+        setPassword,
+        setShowPassword,
+        
+        // Actions
+        handleSubmit,
+        clearError,
+    }
+}
