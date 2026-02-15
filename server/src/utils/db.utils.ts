0608369197f410f50fbfafd8956ba793184dabec
ShlomoSha <shlomoworking1@gmail.com>
2026-02-15 16:31:09 +0200

    I upgraded the function so that it can accept even just 1 parameter.

--- server/src/utils/db.utils.ts
+++ server/src/utils/db.utils.ts
@@ -1,8 +1,15 @@
 import { ConflictError } from "../errors/httpErrors"
 import { UserModel } from "../models/user/user.schema"
 
-export const validateUserUniqueness = async (username: string, email: string) => {
-    const existUser = await UserModel.findOne({ $or: [ {username}, {email} ] })
+export const validateUserUniqueness = async (username?: string, email?: string) => {
+    const conditions = []
+
+    if (username) conditions.push({username})
+    if (email) conditions.push({email})
+
+    if (conditions.length === 0) return
+
+    const existUser = await UserModel.findOne({ $or: conditions })
 
     if (!existUser) return
 
