af706b22eab33699f8f8c3f38900ececc96dd3dd
ShlomoSha <shlomoworking1@gmail.com>
2026-02-15 18:08:33 +0200

    changed the return of the function that return object with available and message

--- server/src/utils/db.utils.ts
+++ server/src/utils/db.utils.ts
@@ -1,18 +1,15 @@
-import { ConflictError } from "../errors/httpErrors"
 import { UserModel } from "../models/user/user.schema"
 
-export const validateUserUniqueness = async (username?: string, email?: string) => {
+export const validateUserUniqueness = async (username?: string, email?: string): Promise<{ available: boolean; message: string; }> => {
     const conditions = []
 
     if (username) conditions.push({username})
     if (email) conditions.push({email})
 
-    if (conditions.length === 0) return
-
     const existUser = await UserModel.findOne({ $or: conditions })
 
-    if (!existUser) return
+    if (!existUser) return { available: true, message: 'There is no user with this details' }
 
     const field = existUser.username === username ? 'username' : 'email'
-    throw new ConflictError(`User with this ${field} already exist`)
+    return { available: false, message: `User with this ${field} already exist` }
 }
