1351a7f64c9580274ab35d41e2aacd700927a906
ShlomoSha <shlomoworking1@gmail.com>
2026-02-15 15:59:08 +0200

    deleted the BrowserRouter from the main and added RouterProvider in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,16 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Tasks from "./pages/Tasks"
-import AppLayout from "./layouts/AppLayout"
-import AuthLayout from "./layouts/AuthLayout"
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createRoutes } from "./routes/routesFactory"
+import appRoute from "./routes/app.routes"
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutes(appRoute)
+  )
+
   return (
     <>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path='/'element={<Navigate to={'login'} />} />
-          <Route element={<AuthLayout />}>
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-          </Route>
-          <Route path='tasks' element={<Tasks />} />
-        </Route>
-      </Routes>
+      <RouterProvider router={router} />
     </>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { CssBaseline } from '@mui/material'
 import '../index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
     <CssBaseline />
       <App />
-    </BrowserRouter>
   </StrictMode>,
 )
