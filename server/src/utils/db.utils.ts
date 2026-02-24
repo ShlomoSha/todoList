ebf53eff595f26fc439caeee3e7e87b993bbfe46
shlomoShalem <s0552288304@gmail.com>
2026-02-24 08:29:48 +0200

    added aupported in excludeUserId

--- server/src/utils/db.utils.ts
+++ server/src/utils/db.utils.ts
@@ -1,12 +1,20 @@
 import { UserModel } from "../models/user/user.schema"
 
-export const validateUserUniqueness = async (username?: string, email?: string): Promise<{ available: boolean; message: string; }> => {
+export const validateUserUniqueness = async (username?: string, email?: string, excludeUserId?: string): Promise<{ available: boolean; message: string; }> => {
     const conditions = []
 
     if (username) conditions.push({username})
     if (email) conditions.push({email})
 
-    const existUser = await UserModel.findOne({ $or: conditions })
+    if (conditions.length === 0) return { available: true, message: 'No fields to check' }
+
+    const query: any = { $or: conditions }
+    
+    if (excludeUserId) {
+        query._id = { $ne: excludeUserId }
+    }
+
+    const existUser = await UserModel.findOne(query)
 
     if (!existUser) return { available: true, message: 'There is no user with this details' }
 
