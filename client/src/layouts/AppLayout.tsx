03a3125fc4d8ad21f7e314a889c5fc1f1ddd4c7f
shlomoShalem <s0552288304@gmail.com>
2026-02-24 08:32:57 +0200

    improve the app layout

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -3,17 +3,14 @@ import { Outlet } from "react-router-dom";
 
 export default function AppLayout() {
   return (
-    <>
-         <Box sx={{
-            height: '100%',
-            width: '100%',
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            bgcolor: "#f5eee8ff"
-        }}>
-            <Outlet />
-        </Box>
-    </>
+    <Box sx={{
+        height: '100%',
+        width: '100%',
+        display: "flex",
+        bgcolor: "#f9f4efff",
+        overflow: "hidden",
+    }}>
+        <Outlet />
+    </Box>
   )
 }
